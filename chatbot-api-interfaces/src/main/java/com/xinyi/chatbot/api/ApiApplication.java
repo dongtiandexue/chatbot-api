aacc32b6d00544463b8fd204f683ac8ca0e45435
xinyi <huawei_code@163.com>
2023-02-11 10:00:00 +0800

    feat: chatbot-api 工程初始化

--- chatbot-api-interfaces/src/main/java/com/xinyi/chatbot/api/ApiApplication.java
+++ chatbot-api-interfaces/src/main/java/com/xinyi/chatbot/api/ApiApplication.java
@@ -1,5 +1,8 @@
 package com.xinyi.chatbot.api;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author xinyi
  * @description 启动入口
